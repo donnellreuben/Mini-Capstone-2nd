ff6c59041b6a0dbbfad7ad7cd060ab36aac2e909
Donnell Reuben <donnell.reuben@icloud.com>
2023-04-17 12:12:23 -0500

    Fixed routes and server issues

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,15 @@ Rails.application.routes.draw do
   get "/products" => "products#index"
   get "/products/:id" => "products#show"
   post "/products" => "products#create"
+  patch "/products/:id" => "products#update"
+  delete "/products/:id" => "products#destroy"
   
+  get "/suppliers" => "products#supplier_index"
+  get "/suppliers/:id" => "products#supplier_show"
+  post "/suppliers" => "products#supplier_create"
+  patch "/suppliers/:id" => "products#supplier_update"
+  delete "/suppliers/:id" => "products#supplier_destroy"
+
   post "/users" => "users#create"
   post "/sessions" => "sessions#create"
 end
