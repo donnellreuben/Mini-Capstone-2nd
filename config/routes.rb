32d7cdfa93488d66e0f2fe9fad7d88bc4b95dc03
Donnell Reuben <donnell.reuben@icloud.com>
2023-04-17 11:43:06 -0500

    Add users create action (signup)

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,5 @@ Rails.application.routes.draw do
   get "/products" => "products#index"
   get "/products/:id" => "products#show"
   post "/products" => "products#create"
-  
-
-
-  # REST API
-  # CRUD through the web + naming conventions
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  post "/users" => "users#create"
 end
